9fe82601218cc7b045601ed7adee7bf91a47d8df
Alma C Castaneda Leautaud <76185363+chemdesign-accl@users.noreply.github.com>
2025-07-22 13:58:48 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,14 +18,14 @@ setup(
         "molvs",
         "mendeleev",
         "scikit-learn",
-        "torch>=2.7.1",
-        "torchvision>=0.21.0+cu128",
-        "torchaudio>=2.7.0+cu128",
-        "pyg-lib==0.4.0+pt27cu128",
-        "torch-scatter==2.1.2+pt27cu128",
-        "torch-sparse==0.6.18+pt27cu128",
-        "torch-cluster==1.6.3+pt27cu128",
-        "torch-spline-conv==1.2.2+pt27cu128"
+        "torch>=2.7.0",
+        "torchvision>=0.21.0",
+        "torchaudio>=2.7.0",
+        "pyg-lib",
+        "torch-scatter",
+        "torch-sparse",
+        "torch-cluster",
+        "torch-spline-conv",
     ],
     python_requires='>=3.11'
 )
